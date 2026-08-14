9ad9d0c0ea51db3cd35808c9446e361fae74fa06
IvarBoneless22 <162493787+IvarBoneless22@users.noreply.github.com>
2026-08-14 11:46:50 +0300

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,9 @@
+window.LISHI_VALIDATION={
+  "Audi":["A1","A2","A3","A4","A5","A6","A7","A8","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","TT","RS5","S3","SQ5"],
+  "BMW":["1 Series","2 Series","3 Series","4 Series","5 Series","6 Series","7 Series","M2","M3","M4","M5","M6","X1","X2","X3","X4","X5","X6","X7","Z4"],
+  "Citroen":["C1","C2","C3","C4","C5","C6","C8","C-ZERO","Berlingo","Jumper","Jumpy","Nemo","Relay","DS3","DS4","DS5","DS7","C3 Aircross","C4 Cactus","C5 Aircross","Dispatch"],
+  "Dacia":["Dokker","Duster","Jogger","Logan","Sandero","Sandero Stepway","Spring"],
+  "Mercedes-Benz":["A-Class","B-Class","C-Class","CL-Class","CLA-Class","CLK-Class","CLS","E-Class","G-Class","GLB","ML","S-Class","SLK","Sprinter","Vito","X-Class"],
+  "Renault":["Arkana","Captur","Clio","Espace","Fluence","Kangoo","Koleos","Laguna","Master","Megane","Scenic","Talisman","Trafic","Twingo"],
+  "Volkswagen":["Amarok","Beetle","Bora","Caddy","Crafter","Eos","Golf","Jetta","Passat","Polo","T-Cross","T-Roc","T5","Tiguan","Touareg","Transporter","Up"]
+};
