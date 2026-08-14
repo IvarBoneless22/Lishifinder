c829cd88090e59cff02007c4fd87d8a70edabc02
IvarBoneless22 <162493787+IvarBoneless22@users.noreply.github.com>
2026-08-14 16:24:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE='lishi-finder-v3-20260814';
+const CACHE='lishi-finder-v5-textonly-20260814';
 const PRECACHE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
 self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
